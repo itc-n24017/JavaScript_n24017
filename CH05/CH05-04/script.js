6bd6fc55df5b9d3545d596fc3841bc264ff462d0
itc-n24017 <n24017@std.it-college.ac.jp>
2024-12-10 16:50:53 +0900

    現在時刻を表示

---
+++ CH05/CH05-04/script.js
@@ -0,0 +1,13 @@
+setInterval(function(){
+    var time_el = document.getElementById('time_el');
+  
+    var date = new Date();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    time_el.innerHTML = hour+':'+min+':'+sec;
+  }, 1000);
+  
+  
+  
+  
